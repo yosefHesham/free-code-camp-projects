5166fafb05455b6537e030eb4a4f5090695f05ca
yosefHesham <yosef.seven1998@gmail.com>
2021-12-29 11:43:20 +0200

    adding css

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 console.log("hello world");
 
-app.use(__dirname + "/public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 // app.get("/", (req, res) => {
 //   res.send("Hello Express");
