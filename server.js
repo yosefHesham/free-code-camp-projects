efe055683fe3d3da2facfa61203d02fec324539a
yosefHesham <yosef.seven1998@gmail.com>
2021-12-29 11:49:16 +0200

    add json2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var bGround = require("fcc-express-bground");
 var myApp = require("./myApp");
 var express = require("express");
 var app = express();
+app.use(express.json());
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function (req, res, next) {
